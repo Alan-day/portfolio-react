10e0a313bfcbed9f7e4f173ef6e956b2870edef4
Alan <lesterthenovice@gmail.com>
2023-09-17 14:34:10 +0100

    footer fixed

--- src/Components/Navbar/Footer/Footer.jsx
+++ src/Components/Navbar/Footer/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Footer.scss";
-
+import images from "./../../../Resources/images/Icons.svg";
 const Footer = () => {
   return (
-    <footer>
-      <div class="footer">
-        <p class="footer__element1">alan.szafarczyk@gmail.com</p>
-        <img class="footer__element2" src="images/Icons.svg" />
-      </div>
-    </footer>
+<footer className="footer">
+  <div className="footer__content">
+    <p className="footer__element1">alan.szafarczyk@gmail.com</p>
+    <img className="footer__element2" src={images} alt="Footer Image" />
+  </div>
+</footer>
   );
 };
 
